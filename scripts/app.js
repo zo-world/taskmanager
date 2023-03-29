301290102e17f218cefb87a2b81e7b46b67e23ef
Yreish Pozo <101380787+zo-world@users.noreply.github.com>
2023-03-28 19:45:48 -0700

    class 2 updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-console.log("Task Manager");
+var isImportant = false;
 
 function togglePanel(){
   $("#form").slideToggle("slow");
@@ -6,14 +6,48 @@ function togglePanel(){
 }
 function saveTask(){
   console.log("Save clicked");
+
+  const title = $("#txtTitle").val(); //document.getElementById("txtTitle").innerHTML
+  const desc = $("#txtDescription").val();
+  const dueDate = $("#selDueDate").val();
+  const duration = $("#txtDuration").val();
+  const status = $("#selStatus").val();
+  const color = $("#selColor").val();
+
+  let task = new Task(title, isImportant, desc, dueDate, duration, status, color);
+  console.log(task);
+  
+  displayTask(task);
+}
+
+function displayTask(task){
+  let syntax = `<div class="task" style="border:1px solid ${task.color};">
+    <div class="info">
+      <h5>${task.title}</h5>
+      <p>${task.description}</p>
+    </div>
+    <label>${task.status}</label>
+    <div class="dates">
+      <label>${task.dueDate}</label>
+      <label>${task.duration}</label>
+    </div>
+  </div>`;
+
+  $("#pendingTasks").append(syntax);
 }
 
 function toggleImportant(){
   const nonImpClasses = "fa-regular fa-bookmark not-important";
   const impClasses = "fa-solid fa-bookmark important";
 
-  $("#iImportant").removeClass(nonImpClasses)
-  $("#iImportant").addClass(impClasses);
+  if(isImportant){
+    $("#iImportant").removeClass(impClasses).addClass(nonImpClasses);
+    isImportant = false;
+  }
+  else {
+    $("#iImportant").removeClass(nonImpClasses).addClass(impClasses);
+    isImportant = true;
+  }
 }
 
 function init(){
@@ -28,3 +62,16 @@ function init(){
 }
 
 window.onload = init;
+
+/**
+ * Inv home:
+ * http methods/verbs
+ * http status codes
+ * 
+ * 
+ * 
+ * Challenges:
+ * - format date
+ * - clear form after displaying
+ * 
+ */
---
+++ scripts/task.js
@@ -0,0 +1,13 @@
+class Task{
+  constructor(title, important, desc, dueDate, duration, status, color){
+    this.title = title;
+    this.important = important;
+    this.description = desc;
+    this.dueDate = dueDate;
+    this.duration = duration;
+    this.status = status;
+    this.color = color;
+
+    this.name = "Yreish";
+  }
+}
